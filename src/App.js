25678e67239f126f4c673b79547870657b1a360d
Deepika <deepikachauhan8988@gmail.com>
2025-03-11 15:25:21 +0530

    awc appjs structure has been done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "../../../AWC/AWC/src/custom/style.css";
+import NavBar from "./componets/topnav/NavBar";
+import Home from "./componets/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Router>
+  <NavBar />
+<Routes>
+<Route path="/" element={<Home />} />
+
+</Routes>
+</Router>
   );
 }
-
+ 
 export default App;
