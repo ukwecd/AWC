85b179d5403bd573eee85b9e6c2335cbaf2fbd84
rajeshthap <ukwecd@gmail.com>
2025-03-11 15:36:24 +0530

    App update

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import "../../../AWC/AWC/src/custom/style.css";
 import NavBar from "./componets/topnav/NavBar";
+import "../../awc/src/custom/style.css";
 import Home from "./componets/Home";
 
 function App() {
   return (
-<Router>
-  <NavBar />
-<Routes>
-<Route path="/" element={<Home />} />
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-</Routes>
-</Router>
+      </Routes>
+    </Router>
   );
 }
- 
+
 export default App;
